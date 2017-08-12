0277c00044f6f9a1567136e3ef48062a42fb1297
Marco MC <marco.munoz@lagence.mx>
2017-08-12 14:36:39 -0500

    feature(userController.js) delete comments

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,16 @@
 const User = require('../models/user.js');
 
-//Mostrar toda la lista de contactos
+// SHOW ALL
 exports.list_all = (req, res) => {
 	User.list_all(res);
 }
 
-// Show contacto
+// SHOW DETAIL
 exports.user_detail = (req, res) => {
   User.show(req, res, req.params.id);  
 }
 
-//Create contacto POST
+// CREATE
 exports.user_create_post = (req, res) => {
   User.create(res, req.body.name, req.body.phone, req.body.avatar);
 }
@@ -18,9 +18,4 @@ exports.user_create_post = (req, res) => {
 // DELETE
 exports.user_delete_get = (req, res) => {
   User.delete(req, res, req.params.id);
-};
-
-// DETAIL
-exports.user_update = function(req, res) {
-    res.send('UPDATE CONTACT');
 };
